3348299eeeeefc967bb50a160aea58b0004dc2c1
Love6272fon <mjongpra@gmail.com>
2026-04-24 18:23:20 +0700

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,23 @@
+num1 = int(input("Give me a random number"))
+num2 = int(input("Give me a second random number"))
+
+def subtract(num1, num2):
+    subtract = num1 - num2
+    print(subtract)
+#subtract(num1,num2)
+def multiply(num1, num2):
+    multiply = num1 * num2
+    print(multiply)
+#multiply(num1, num2)
+def divide(num1,num2):
+    divide = num1/num2
+    print(divide)
+#divide(num1,num2)
+def power(num1,num2):
+    power=num1**num2
+    print(power)
+#power(num1, num2)
+def remainder(num1,num2):
+    remainder=num1%num2
+    print(remainder)
+remainder(num1,num2)
